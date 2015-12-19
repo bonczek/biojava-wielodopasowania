ca0a538b316a087e2bccac27905d4f9abca6548e
Bonczek <adam-bak@o2.pl>
2015-12-19 16:33:07 +0100

    wersja konsolowa wielodopasowania z wykladowego przykladu

---
+++ src/test/java/application/MainTest.java
@@ -0,0 +1,18 @@
+package application;
+
+import org.testng.annotations.Test;
+
+public class MainTest {
+
+    @Test
+    public void testMain() throws Exception {
+        String[] input = {};
+        Main.main(input);
+    }
+
+    @Test
+    public void testMainWithRawSequences() throws Exception {
+        String[] input = {"-r", "ABCA", "ABABA", "ACCB", "CBBC"};
+        Main.main(input);
+    }
+}
